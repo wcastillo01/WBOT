e91a662434605f9369ec5fdc48eb6b4dbb150ecb
wcastillo01 <wcastillopujols1@gmail.com>
2021-03-16 00:11:27 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+
+token = read_token()
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.find("!hello") !=-1:
+        await message.channel.send("Hello creator")
+
+client.run(token)
